809eb32a9bbe7215d4d16d0032ec15771291c938
Matt Macy <mmacy@torch.rs>
2017-02-05 22:08:10 -0800

    start unpacking metadata in the dicom file

--- src/filereader.rs
+++ src/filereader.rs
@@ -1,6 +1,7 @@
 
 
 //use std::collections::HashMap;
+use std::mem;
 use std::str;
 use std::path::Path;
 use std::io::Result;
@@ -16,14 +17,30 @@ use dicom_types::DicomObject;
 // pub fn read_partial<'a>(std::io::Bufread &rd) -> &str<'a> { 
     
 // }
-
+#[derive(Debug)]
+#[repr(C, packed)]
+struct MetaInfo {
+    group: u16,
+    elem: u16,
+    vr: [u8; 2],
+    length: u16,
+}
+const EXTRA_LENGTH_VRS:[&'static str; 6] = ["OB", "OW", "OF", "SQ", "UN", "UT"];
+const TEXT_VRS:[&'static str; 6] = ["SH", "LO", "ST", "LT",  "UR", "UT"];  // and PN, but it is handled separately.
 
 pub fn parse<'a, P>(path: P) -> Result<DicomObject> where P : AsRef<Path> {
-    let pre_off = 0x80;
+    let mut off = 0x80;
     let file_mmap = Mmap::open_path(path, Protection::Read).expect("mmap fail");
-    let bytes: &[u8] = unsafe { file_mmap.as_slice() };
+    let data: &[u8] = unsafe { file_mmap.as_slice() };
+    let magic = str::from_utf8(&data[off..off+4]).unwrap();
     
-    if str::from_utf8(&bytes[pre_off..pre_off+4]).unwrap() != "DICM" { panic!("bad magic in header"); }
+    if magic != "DICM" { panic!("bad magic in header"); };
+    off += 4;
+    let mi : &MetaInfo = unsafe {
+        mem::transmute::<*const u8, &MetaInfo>(data[off..off+8].as_ptr())
+    };
+    let vr = str::from_utf8(&mi.vr[0..2]).unwrap();
+    println!("magic: {} mi: {:?} vr: {} invrs: {}", magic, mi, vr, EXTRA_LENGTH_VRS.contains(&vr));
 
     Ok(DicomObject {})
 }
